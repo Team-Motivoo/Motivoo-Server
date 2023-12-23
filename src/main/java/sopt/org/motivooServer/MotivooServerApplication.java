4177a7ce1c64f18babaff583f85a009761b4af20
jun02160 <jun020216@naver.com>
2023-12-24 02:08:32 +0900

    [INIT] 프로젝트 초기 세팅

---
+++ src/main/java/sopt/org/motivooServer/MotivooServerApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.motivooServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivooServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivooServerApplication.class, args);
+	}
+
+}
