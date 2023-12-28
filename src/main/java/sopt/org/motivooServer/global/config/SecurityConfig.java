e7d83a09f585525a5e9c4bf241f46d51916c2e1a
jun02160 <jun020216@naver.com>
2023-12-28 22:57:22 +0900

    🔧 chore: 설정 파일 구성 #1

---
+++ src/main/java/sopt/org/motivooServer/global/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package sopt.org.motivooServer.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedOriginPatterns("*")
+					.allowedMethods("*");
+			}
+		}
+	}
+}
