8908029d1efc458c0b608634d1acdd73ad78b12e
jun02160 <jun020216@naver.com>
2024-01-03 00:36:16 +0900

    🐛 bugfix: 테스트에서의 슬랙 의존성 문제 해결 #4

--- src/test/java/sopt/org/motivooServer/MotivooServerApplicationTests.java
+++ src/test/java/sopt/org/motivooServer/MotivooServerApplicationTests.java
@@ -2,9 +2,15 @@ package sopt.org.motivooServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import sopt.org.motivooServer.global.util.slack.SlackUtil;
 
 @SpringBootTest
 class MotivooServerApplicationTests {
+
+	@MockBean
+	private SlackUtil slackUtil;
 
 	@Test
 	void contextLoads() {
