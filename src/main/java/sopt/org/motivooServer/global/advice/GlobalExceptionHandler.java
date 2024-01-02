41d3b63dc3a95ec11b30284b161dfe4ed972b297
jun02160 <jun020216@naver.com>
2024-01-02 19:28:35 +0900

    ✨ feat: 슬랙 알림 추가 #4

--- src/main/java/sopt/org/motivooServer/global/advice/GlobalExceptionHandler.java
+++ src/main/java/sopt/org/motivooServer/global/advice/GlobalExceptionHandler.java
@@ -1,14 +1,28 @@
 package sopt.org.motivooServer.global.advice;
 
+import static org.springframework.http.HttpStatus.*;
+import static sopt.org.motivooServer.global.advice.ErrorType.INTERNAL_SERVER_ERROR;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import sopt.org.motivooServer.global.util.slack.SlackUtil;
 
 @Slf4j
+@Component
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	private final SlackUtil slackUtil;
 
 	/**
 	 * 400 Bad Request
@@ -18,7 +32,12 @@ public class GlobalExceptionHandler {
 	/**
 	 * 500 Internal Server Error
 	 */
-
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(final Exception e, final HttpServletRequest request) throws IOException {
+		slackUtil.sendAlert(e, request);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(ErrorResponse.of(INTERNAL_SERVER_ERROR));
+	}
 
 	/**
 	 * Custom Error
--- src/main/java/sopt/org/motivooServer/global/healthcheck/HealthCheck.java
+++ src/main/java/sopt/org/motivooServer/global/healthcheck/HealthCheck.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import sopt.org.motivooServer.global.response.ApiResponse;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/api/health")
 public class HealthCheck implements HealthCheckApi {
 
 	@GetMapping
@@ -21,7 +21,7 @@ public class HealthCheck implements HealthCheckApi {
 	}
 
 	// @ResponseStatus VS ResponseEntity
-	@GetMapping
+	@GetMapping("/v2")
 	@ResponseStatus(HttpStatus.OK)
 	public ApiResponse<String> healthCheckV2() {
 		return ApiResponse.successV2(HEALTH_CHECK_SUCCESS);
