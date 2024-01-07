c8d9a2e6b6d96c7146904f22761134dfa35d1235
jun02160 <jun020216@naver.com>
2024-01-07 17:48:28 +0900

    🚑 hotfix: 배포 스크립트 동작 오류 해결

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ SERVER_NAME=motivoo-server
 
 DOCKER_PS_OUTPUT=$(docker ps | grep $SERVER_NAME)
 RUNNING_CONTAINER_NAME=$(echo "$DOCKER_PS_OUTPUT" | awk '{print $NF}')
-RUNNING_SERVER_PORT=$(echo "$RUNNING_CONTAINER_NAME" | awk -F'-' '{print $NF}')
 
 IS_REDIS_ACTIVATE=$(docker ps | grep redis)
 
@@ -23,14 +22,19 @@ if [ -z "$IS_REDIS_ACTIVATE" ];then
 fi
 
 # 실행 중인 서버 포트 확인
-if [ -z "$RUNNNG_SERVER_PORT" ]; then
-    echo "[$NOW_TIME] 실행 중인 서버 포트: 없음"
+if [ ${RUNNING_CONTAINER_NAME} == "blue" ]; then
+    echo "[$NOW_TIME] 실행 중인 서버 포트: blue (:8080)"
+    RUNNING_SERVER_PORT=8080
+elif [ ${RUNNING_CONTAINER_NAME} == "green" ]; then
+    echo "[$NOW_TIME] 실행 중인 서버 포트: green (:8081)"
+    RUNNING_SERVER_PORT=8081
 else
-    echo "[$NOW_TIME] 실행 중인 서버 포트: $RUNNING_SERVER_PORT"
+  echo "[$NOW_TIME] 실행 중인 서버 포트: 없음"
 fi
 
+
 # 현재 실행 중인 포트 외 실행가능한 포트 확인
-for iten in "${ALL_PORT[@]}"; do
+for item in "${ALL_PORTS[@]}"; do
   if [ "$item" != "$RUNNING_SERVER_PORT" ]; then
     AVAILABLE_PORT+=("$item")
   fi
@@ -39,15 +43,17 @@ done;
 # 실행 가능한 포트 없으면 끝내기
 if [ ${#AVAILABLE_PORT[@]} -eq 0 ]; then
     echo "[$NOW_TIME] 실행 가능한 포트가 없습니다."
-    exit 1
+#    exit 1
 fi
 
 # Green Up
-if [ $RUNNING_SERVER_PORT -eq 8080 ]; then
+if [ $RUNNING_CONTAINER_NAME == "blue" ]; then
   echo "[$NOW_TIME] 현재 구동중인 Port: Blue(:8080)"
 
   echo "[$NOW_TIME] ###### 스위칭 ######"
   echo "[$NOW_TIME] ###### BLUE -> GREEN ######"
+  CURRENT_SERVER_PORT=8081
+
   echo "[$NOW_TIME] Green 도커 이미지 pull"
   docker-compose pull green
   echo "[$NOW_TIME] Green 컨테이너 Up (빌드 & 실행)"
@@ -57,7 +63,7 @@ if [ $RUNNING_SERVER_PORT -eq 8080 ]; then
     echo "[$NOW_TIME] Green health check ..."
     sleep 3
 
-    RESPONSE=$(curl -s http://localhost:${RUNNING_SERVER_PORT}${WEB_HEALTH_CHECK_URL})
+    RESPONSE=$(curl -s http://localhost:8081${WEB_HEALTH_CHECK_URL})
     UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
     echo "[$NOW_TIME] Health check 응답: ${RESPONSE}"
 
@@ -79,7 +85,7 @@ if [ $RUNNING_SERVER_PORT -eq 8080 ]; then
   echo "----------------------------------------------------------------------"
 
   echo "[$NOW_TIME] Nginx Reload (Port 스위칭 적용)"
-  echo "set \$service_url http://127.0.0.1:${RUNNING_SERVER_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  sudo cp /etc/nginx/conf.d/green-url.inc /etc/nginx/conf.d/service-url.inc
   sudo nginx -s reload
   echo "[$NOW_TIME] Blue 컨테이너 중단"
   docker-compose stop blue
@@ -90,6 +96,8 @@ else
 
   echo "[$NOW_TIME] ###### 스위칭 ######"
   echo "[$NOW_TIME] ###### GREEN -> BLUE ######"
+  CURRENT_SERVER_PORT=8080
+
   echo "[$NOW_TIME] Blue 도커 이미지 pull"
   docker-compose pull blue
   echo "[$NOW_TIME] Blue 컨테이너 Up (빌드 & 실행)"
@@ -99,7 +107,7 @@ else
     echo "[$NOW_TIME] Blue health check ..."
     sleep 3
 
-    RESPONSE=$(curl -s http://localhost:${RUNNING_SERVER_PORT}/actuator/health)
+    RESPONSE=$(curl -s http://localhost:8080${RUNNING_SERVER_PORT}/actuator/health)
     UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
     echo "[$NOW_TIME] Health check 응답: ${RESPONSE}"
 
@@ -121,7 +129,7 @@ else
   echo "----------------------------------------------------------------------"
 
   echo "[$NOW_TIME] Nginx Reload (Port 스위칭 적용)"
-  echo "set \$service_url http://127.0.0.1:${RUNNING_SERVER_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  sudo cp /etc/nginx/conf.d/blue-url.inc /etc/nginx/conf.d/service-url.inc
   sudo nginx -s reload
   echo "[$NOW_TIME] Green 컨테이너 중단"
   docker-compose stop green
@@ -130,7 +138,7 @@ fi
 echo "----------------------------------------------------------------------"
 
 # Nginx를 통해서 서버에 접근 가능한지 확인
-RESPONSE=$(curl -s http://localhost:${RUNNING_SERVER_PORT}/actuator/health)
+RESPONSE=$(curl -s http://localhost:${CURRENT_SERVER_PORT}${WEB_HEALTH_CHECK_URL})
 UP_COUNT=$(echo $RESPONSE | grep 'UP' | wc -l)
 echo "[$NOW_TIME] Health check 응답: ${RESPONSE}"
 
@@ -141,11 +149,5 @@ else
     echo "> 서버 변경 실패"
     echo "> 서버 응답 결과: ${RESPONSE}"
     exit 1
-fi
-
-# 스위칭 이전의 서버 있다면 중단
-if [ -n "$RUNNING_SERVER_PORT" ]; then
-    echo "> 기존 ${RUNNING_SERVER_PORT}포트 서버 중단"
-    sudo docker rm -f ${SERVER_NAME}-${RUNNING_SERVER_PORT}
 fi
 
