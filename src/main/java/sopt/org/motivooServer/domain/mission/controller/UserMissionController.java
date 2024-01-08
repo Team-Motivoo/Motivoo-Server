f74a2beb4100fde777cceab86d876e58c1515086
jun02160 <jun020216@naver.com>
2024-01-08 22:43:27 +0900

    ✨ feat: 미션 인증사진 등록 Vallidation 추가 #15

--- src/main/java/sopt/org/motivooServer/domain/mission/controller/UserMissionController.java
+++ src/main/java/sopt/org/motivooServer/domain/mission/controller/UserMissionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import sopt.org.motivooServer.domain.mission.dto.request.MissionImgUrlRequest;
 import sopt.org.motivooServer.domain.mission.dto.response.MissionImgUrlResponse;
@@ -24,7 +25,7 @@ public class UserMissionController {
 
 	// TODO 항상 유저의 오늘의 미션에만 인증 사진 등록이 가능하므로, @PathVariable은 생략할 예정
 	@PatchMapping("/image/{missionId}")
-	public ResponseEntity<ApiResponse<MissionImgUrlResponse>> getMissionImgUrl(@RequestBody final MissionImgUrlRequest request,
+	public ResponseEntity<ApiResponse<MissionImgUrlResponse>> getMissionImgUrl(@Valid @RequestBody final MissionImgUrlRequest request,
 																			@PathVariable final Long missionId) {
 		return ApiResponse.success(GET_MISSION_IMAGE_PRE_SIGNED_URL_SUCCESS,
 			userMissionService.getMissionImgUrl(request, missionId));
