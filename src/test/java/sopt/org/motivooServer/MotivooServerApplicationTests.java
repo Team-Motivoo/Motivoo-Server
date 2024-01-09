017e06ce153d015bb98e59fbc6c5b64104984a75
jun02160 <jun020216@naver.com>
2024-01-09 13:49:48 +0900

    🔧 chore: 불필요한 의존성 주입 제거, 테스트 환경 추가 #16

--- src/test/java/sopt/org/motivooServer/MotivooServerApplicationTests.java
+++ src/test/java/sopt/org/motivooServer/MotivooServerApplicationTests.java
@@ -2,17 +2,11 @@ package sopt.org.motivooServer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import sopt.org.motivooServer.global.util.slack.SlackUtil;
-
 @SpringBootTest
-@ActiveProfiles({"deploy", "local"})
+@ActiveProfiles({"test"})
 class MotivooServerApplicationTests {
-
-	@MockBean
-	private SlackUtil slackUtil;
 
 	@Test
 	void contextLoads() {
