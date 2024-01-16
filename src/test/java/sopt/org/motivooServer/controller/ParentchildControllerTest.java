da53fc2504c225a82420a0aab5a52096d153bdb5
이혜연 <tjsalsu@naver.com>
2024-01-16 22:33:46 +0900

    ✅ test: 온보딩 정보 입력 확인 테스트 추가

--- src/test/java/sopt/org/motivooServer/controller/ParentchildControllerTest.java
+++ src/test/java/sopt/org/motivooServer/controller/ParentchildControllerTest.java
@@ -8,11 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.mockmvc.*;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import sopt.org.motivooServer.domain.health.dto.request.OnboardingRequest;
+import sopt.org.motivooServer.domain.health.dto.response.CheckOnboardingResponse;
 import sopt.org.motivooServer.domain.health.dto.response.OnboardingResponse;
 import sopt.org.motivooServer.domain.parentchild.controller.ParentChildController;
 import sopt.org.motivooServer.domain.parentchild.repository.ParentchildRepository;
@@ -30,9 +30,11 @@ import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static sopt.org.motivooServer.global.response.SuccessType.CHECK_ONBOARDING_INFO_SUCCESS;
 import static sopt.org.motivooServer.global.response.SuccessType.ONBOARDING_SUCCESS;
 import static sopt.org.motivooServer.util.ApiDocumentUtil.getDocumentRequest;
 import static sopt.org.motivooServer.util.ApiDocumentUtil.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 
 @Slf4j
 @DisplayName("ParentchildController 테스트")
@@ -40,7 +42,7 @@ import static sopt.org.motivooServer.util.ApiDocumentUtil.getDocumentResponse;
 public class ParentchildControllerTest extends BaseControllerTest {
 
     private static final String DEFAULT_URL = "/user";
-    private final String TAG = "";
+    private final String TAG = "parentchild";
 
     @MockBean
     ParentChildController parentChildController;
@@ -84,10 +86,10 @@ public class ParentchildControllerTest extends BaseControllerTest {
                                         .tag(TAG)
                                         .description("온보딩시 유저와 운동 관련 정보를 입력하는 API")
                                         .requestFields(
-                                                fieldWithPath("type").type(STRING).description("유저 타입(자녀 or 부모"),
+                                                fieldWithPath("type").type(STRING).description("유저 타입(자녀|부모"),
                                                 fieldWithPath("age").type(NUMBER).description("나이"),
                                                 fieldWithPath("is_exercise").type(BOOLEAN).description("운동 유무"),
-                                                fieldWithPath("exercise_type").type(STRING).description("운동 유형(고강도/중강도/저강도"),
+                                                fieldWithPath("exercise_type").type(STRING).description("운동 유형(고강도|중강도|저강도"),
                                                 fieldWithPath("exercise_count").type(STRING).description("운동 일수"),
                                                 fieldWithPath("exercise_time").type(STRING).description("운동 시간"),
                                                 fieldWithPath("exercise_note").type(JsonFieldType.ARRAY).description("운동 유의사항")
@@ -99,10 +101,46 @@ public class ParentchildControllerTest extends BaseControllerTest {
                                                 fieldWithPath("data").description("응답 데이터"),
                                                 fieldWithPath("data.user_id").type(LONG).description("유저 아이디"),
                                                 fieldWithPath("data.invite_code").type(STRING).description("초대 코드"),
-                                                fieldWithPath("data.exercise_level").type(STRING).description("유저 분류(초보/중수/고수")
+                                                fieldWithPath("data.exercise_level").type(STRING).description("유저 분류(초보|중수|고수")
                                                 ).build()
                                         )
                 )).andExpect(MockMvcResultMatchers.status().isCreated()); //201 OK
     }
+
+    @Test
+    @DisplayName("온보딩 정보 입력 확인 테스트")
+    void checkOnboardingInfo() throws Exception {
+        //given
+        CheckOnboardingResponse response = new CheckOnboardingResponse(true);
+        ResponseEntity<ApiResponse<CheckOnboardingResponse>> result = ApiResponse
+                .success(CHECK_ONBOARDING_INFO_SUCCESS, response);
+        //when
+        when(parentChildController.checkOnboardingInfo(principal)).thenReturn(result);
+
+        //then
+        mockMvc.perform(get(DEFAULT_URL+"/onboarding")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .principal(principal)
+        ).andDo(
+                document("온보딩 정보 입력 확인 Example",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .tag(TAG)
+                                        .description("이전에 온보딩 정보를 입력한 이력이 있는지 여부를 알려주는 API")
+                                        .requestFields()
+                                        .responseFields(
+                                                fieldWithPath("code").type(NUMBER).description("상태 코드"),
+                                                fieldWithPath("message").type(JsonFieldType.STRING).description("상태 메세지"),
+                                                fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("응답 성공 여부"),
+                                                fieldWithPath("data").description("응답 데이터"),
+                                                fieldWithPath("data.is_finished_onboarding").type(BOOLEAN).description("유저의 타입(PARENT|CHILD)"))
+                                        .build()
+                        )
+                )).andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
 
 }
