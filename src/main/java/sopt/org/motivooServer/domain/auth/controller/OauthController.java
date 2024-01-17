58c5ed7a2b69fbc06c8310fc446879693405e865
이혜연 <tjsalsu@naver.com>
2024-01-17 17:45:57 +0900

    🐛 bugfix: json 에러 해결

--- src/main/java/sopt/org/motivooServer/domain/auth/controller/OauthController.java
+++ src/main/java/sopt/org/motivooServer/domain/auth/controller/OauthController.java
@@ -1,6 +1,5 @@
 package sopt.org.motivooServer.domain.auth.controller;
 
-import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
