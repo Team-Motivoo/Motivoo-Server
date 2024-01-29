7abcfe0c809120c2dd4536acc3a53e2444c00730
jun02160 <jun020216@naver.com>
2024-01-29 13:55:22 +0900

    ✨ feat: 로그 수집을 위한 Sentry 연동 #111

--- src/main/java/sopt/org/motivooServer/global/healthcheck/SlackTestController.java
+++ src/main/java/sopt/org/motivooServer/global/healthcheck/SlackTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.sentry.Sentry;
 import sopt.org.motivooServer.global.response.ApiResponse;
 
 @RestController
@@ -17,5 +18,18 @@ public class SlackTestController {
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<ApiResponse<Object>> exceptionTest() {
 		throw new IllegalArgumentException();
+	}
+
+
+	//== Sentry 로깅 테스트 ==//
+	@GetMapping("/sentry")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> exceptionTestV2() {
+		try {
+			throw new Exception("This is a test.");
+		} catch (Exception e) {
+			Sentry.captureException(e);
+		}
+		return ResponseEntity.ok("Success Send to Sentry!");
 	}
 }
