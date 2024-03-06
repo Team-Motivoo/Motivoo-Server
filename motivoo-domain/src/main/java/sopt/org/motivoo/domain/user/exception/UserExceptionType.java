eba5b7a2e7634d42627be0fbb20852a4ce2cf07b
jun02160 <jun020216@naver.com>
2024-03-07 00:21:17 +0900

    ⚡️ fix: 매칭된 유저가 탈퇴하는 경우에 대한 HTTP Status 변경

--- motivoo-domain/src/main/java/sopt/org/motivoo/domain/user/exception/UserExceptionType.java
+++ motivoo-domain/src/main/java/sopt/org/motivoo/domain/user/exception/UserExceptionType.java
@@ -17,7 +17,6 @@ public enum UserExceptionType implements BusinessExceptionType {
 	NULL_VALUE_USERTYPE_ENUM(HttpStatus.BAD_REQUEST, "데이터베이스의 유저 타입이 유효하지 않은 값입니다."),
 	NULL_VALUE_AGE(HttpStatus.BAD_REQUEST, "유저의 나이는 null이어서는 안 됩니다."),
 	ALREADY_WITHDRAW_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 유저입니다."),
-	ALREADY_WITHDRAW_OPPONENT_USER(HttpStatus.BAD_REQUEST, "상대 유저가 탈퇴하여 요청을 처리할 수 없습니다."),
 
 	/**
 	 * 401 Unauthorized
@@ -36,7 +35,7 @@ public enum UserExceptionType implements BusinessExceptionType {
 	/**
 	 * 412 Precondition Failed
 	 */
-	// ALREADY_WITHDRAW_OPPONENT_USER(HttpStatus.PRECONDITION_FAILED, "상대 유저가 탈퇴하여 요청을 처리할 수 없습니다."),
+	ALREADY_WITHDRAW_OPPONENT_USER(HttpStatus.PRECONDITION_FAILED, "상대 유저가 탈퇴하여 요청을 처리할 수 없습니다."),
 
 	;
 
